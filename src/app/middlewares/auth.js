148d63a24159c9ae2043d9ebe6a63f23b45de328
Igor Cezatte <igorcezatte13@gmail.com>
2020-02-18 20:21:48 -0300

    Middleware for authenticate

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+import User from '../models/User';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    const userLogged = await User.findOne({
+      where: { id: req.userId },
+    });
+
+    if (!userLogged.admin) {
+      return res.status(400).json({ error: 'Only administrator can do this' });
+    }
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+
+  return next();
+};
