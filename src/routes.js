f84045e1eda5e013a55583cbad0417130ed05e8a
Igor Cezatte <igorcezatte13@gmail.com>
2020-02-19 23:45:08 -0300

    adjusts to new routes and models config

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -18,5 +23,11 @@ routes.put('/users', UserController.update);
 routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.get('/deliverymen', DeliverymanController.index);
+routes.post('/deliverymen', DeliverymanController.store);
+routes.put('/deliverymen/:id', DeliverymanController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 module.exports = routes;
