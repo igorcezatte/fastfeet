59e6e493d0a1693d7b15fc729c9fa41d86af46da
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-03 23:45:46 -0300

    environment variables

--- API/src/config/mail.js
+++ API/src/config/mail.js
@@ -1,9 +1,9 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '5acd9d75217642',
-    pass: '30645a0c8bacaa',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
 };
