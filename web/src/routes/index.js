057f6baaa74bb826bb4b757a2724b5978ff6c76c
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-06 00:00:12 -0300

    private routes + prop-types

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import Deliverymen from '../pages/Deliverymen';
@@ -11,7 +12,7 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/deliverymen" component={Deliverymen} />
+      <Route path="/deliverymen" component={Deliverymen} isPrivate />
       <Route path="/recipients" component={Recipients} />
       <Route path="/deliveries" component={Deliveries} />
       <Route path="/problems" component={Problems} />
