f8f7097f991f8d9b0ab5303c8529612499ba7c55
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-08 18:08:36 -0300

    auth sagas

--- web/src/store/modules/auth/sagas.js
+++ web/src/store/modules/auth/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import history from '~/services/history';
+import { signInSuccess } from './actions';
+
+import api from '~/services/api';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.admin) {
+    console.tron.error('Usuário não autorizado');
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/deliveries');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
