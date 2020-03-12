64bfc47ab4aee90ac754975e9228ddd308d14895
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-12 18:15:01 -0300

    add toast message to failures sign ins

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { ToastContainer } from 'react-toastify';
+
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
@@ -19,6 +22,7 @@ function App() {
         <Router history={history}>
           <Routes />
           <GlobalStyle />
+          <ToastContainer autoClose={3000} />
         </Router>
       </PersistGate>
     </Provider>
