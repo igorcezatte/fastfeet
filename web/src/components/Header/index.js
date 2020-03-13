606ff727ebcb85fe9f36ba8a547ab13a6134e463
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-12 23:57:35 -0300

    Create header component

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo_header.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="FastFeet" />
+          <Link to="/deliveries">ENCOMENDAS</Link>
+          <Link to="/deliverymen">ENTREGADORES</Link>
+          <Link to="/recipients">DESTINATÁRIOS</Link>
+          <Link to="/problems">PROBLEMAS</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <strong>Nome usuário logado</strong>
+            <button type="button">sair do sistema</button>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
