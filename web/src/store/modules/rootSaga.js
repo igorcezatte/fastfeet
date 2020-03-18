904d9ea9de83b6c0c969ecf8f7daa90ecae907af
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-17 22:23:47 -0300

    add reducer from users

--- web/src/store/modules/rootSaga.js
+++ web/src/store/modules/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import auth from './auth/sagas';
+import user from './auth/sagas';
 
 export default function* reduxSaga() {
-  return yield all([auth]);
+  return yield all([auth, user]);
 }
