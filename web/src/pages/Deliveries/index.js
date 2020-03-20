62e0289f8c4698a2c55d911919bd7bde1160f673
Igor Cezatte <igorcezatte13@gmail.com>
2020-03-19 21:32:45 -0300

    Header added on Deliveries page

--- web/src/pages/Deliveries/index.js
+++ web/src/pages/Deliveries/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdSearch, MdAdd } from 'react-icons/md';
+
+import PageHeader from '~/components/PageHeader';
+import { Container, Content } from './styles';
 
 export default function Deliveries() {
-  return <div />;
+  return (
+    <>
+      <Container>
+        <Content>
+          <PageHeader>
+            <h1>Gerenciamento de Encomendas</h1>
+            <div>
+              <div>
+                <MdSearch size={20} color="#999" />
+                <input placeholder="Buscar por encomendas" />
+              </div>
+              <button type="button">
+                <MdAdd size={20} color="#fff" />
+                CADASTRAR
+              </button>
+            </div>
+          </PageHeader>
+        </Content>
+      </Container>
+    </>
+  );
 }
---
+++ web/src/pages/Deliveries/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 0 120px;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  width: 1200px;
+`;
